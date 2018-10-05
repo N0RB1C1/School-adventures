ff35d7079942e51628bc1815eedf88096729a281
N0RB1C1 <gilanyi.norb@gmail.com>
2018-10-05 18:07:08 +0200

    Maxmeghatározás

--- School_Adventure/Program.cs
+++ School_Adventure/Program.cs
@@ -8,11 +8,30 @@ namespace School_Adventure
 {
     class Program
     {
+
+        private static int[] szamok = new int[10];
+
         static void Main(string[] args)
         {
-
-
-
+            int beker;
+            int max = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Adja meg a/az {0}. számot:",i+1);
+                beker = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n");
+                szamok[i] = beker;
+                 
+            }
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if(szamok[i]> max)
+                {
+                    max = szamok[i];
+                }
+            }
+            Console.Write("A legnagyobb elem: {0}", max);
+            Console.ReadKey();
 
         }
     }
